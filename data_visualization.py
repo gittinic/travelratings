4c3f9f2ae56c06a694584cfd0a70e4bb4e986e37
gittinic <37676115+gittinic@users.noreply.github.com>
2018-04-25 01:52:28 -0400

    data_visualization provides bar charts for each feature in the dataset

---
+++ data_visualization.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+feat = pd.read_excel('features.xlsx', sheet_name='Features', header=0)
+
+def bar_plot_score(score, top):
+    # Sort and subset data
+    feat_sort = feat.sort_values(by=[score], ascending=False)
+    feat_subset = feat_sort[:top]
+    ind = np.arange(top)
+
+    # Create bar
+    plt.figure(figsize=(15, 10))
+    plt.bar(ind, feat_subset[score])
+
+    # Plot labeling
+    plt.title('Top ' + str(top) + ' Countries regarding ' + score)
+    plt.ylabel('Score')
+    plt.xticks(ind, feat_subset['Country'], rotation=45)
+    plt.ylim([0, 10])
+    for i, v in enumerate(feat_subset[score]):
+        plt.text(i, v + .25, str(v), color='black')
+    plt.show()
+
+
+def stacked_bar_plot_total_score(top):
+    # Sort and subset data
+    feat_sort = feat.sort_values(by=['Total'], ascending=False)
+    feat_subset = feat_sort[:top]
+    ind = np.arange(top)
+
+    y1 = feat_subset['Affordability']
+    y2 = feat_subset['Safety']
+    y3 = feat_subset['Tourism']
+
+    # Create bars
+    plt.figure(figsize=(15, 10))
+    p1 = plt.bar(ind, y1, color='#F08080')
+    p2 = plt.bar(ind, y2, color='#F0E68C', bottom=y1)
+    p3 = plt.bar(ind, y3, color='#8FBC8B', bottom=y1+y2)
+
+    # Plot labeling
+    for i, v in enumerate(feat_subset['Total']):
+        plt.text(i, v + .5, str(v), color='black')
+    plt.ylabel('Scores')
+    plt.title('Top ' + str(top) + ' Countries regarding ' + 'Aggregated Score')
+    plt.xticks(ind, feat_subset['Country'], rotation=45)
+    plt.ylim([0, 30])
+    plt.legend((p1[0], p2[0], p3[0]), ('Affordability', 'Safety', 'Tourism'))
+    plt.show()
